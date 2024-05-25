11e4c54aafcce99af4af778e8458e5d8cd80bae4
0xngmi <0xngmi@protonmail.com>
2024-05-25 04:36:26 +0100

    apply [L-01]

--- contracts/SmolRefuel.sol
+++ contracts/SmolRefuel.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract SmolRefuel is Ownable {
+contract SmolRefuel is Ownable2Step {
     using SafeERC20 for ERC20Permit;
     using SafeERC20 for IERC20;
 
